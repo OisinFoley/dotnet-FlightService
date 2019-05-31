dd3a4e79d76857c6be8aad2a3423c07ef82323cf
OisinFoley <oisinfoley@yahoo.co.uk>
2019-05-31 15:10:16 +0100

    Added /client directory to serve as autorest client dll for the FlightService

---
+++ client/FlightService.Client/FlightService.Client/FlightService.Client/FlightServiceClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlightService.Client
+{
+    public partial class FlightsServiceClient : FlightServiceAPI
+    {
+        public FlightsServiceClient()
+        {
+            BaseUri = new Uri("http://localhost:5000");
+        }
+    }
+}
